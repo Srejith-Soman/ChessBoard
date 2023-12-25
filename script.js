3d612038703c53ce2ce9e53ecbd0dc0cb62e42a1
sreejith8129 <139565025+sreejith8129@users.noreply.github.com>
2023-12-25 12:37:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const gameboard=document.querySelector(".board")
+
+let white=true
+
+for(let i=1;i<=64;i++){
+    let square=document.createElement("div")
+    square.classList.add("square")
+    if(!white){
+        square.classList.add("black")
+    }
+    white=!white
+    if(i%8===0){
+        white=!white
+    }
+    gameboard.appendChild(square)
+}
